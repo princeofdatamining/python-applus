4b19c96aca9c3672382b5c84bd30bf7d11b428ae
Ji Peng <jipeng@cnicg.cn>
2018-09-29 16:32:50 +0800

    docs: add LICENSE

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Licensed under the MIT License (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+"""
 from distutils.core import setup
 from setuptools import find_packages
 
@@ -14,5 +24,6 @@ setup(name=app_name,
       description=app_description,
       version=app_version,
       url="https://github.com/princeofdatamining/"+app_name,
+      license="MIT License",
       packages=find_packages(exclude=["tests"]),
       install_requires=app_requires)
